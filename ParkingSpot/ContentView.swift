3a12e148a668fd9af6b4c1c343e6ca2791040ef2
Yael Zamora <ajedrezkasparov@gmail.com>
2024-11-20 16:57:34 -0600

    Initial Commit

---
+++ ParkingSpot/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParkingSpot
+//
+//  Created by Yael Javier Zamora Moreno on 20/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParkingSpot/ParkingSpotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingSpotApp.swift
+//  ParkingSpot
+//
+//  Created by Yael Javier Zamora Moreno on 20/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkingSpotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
